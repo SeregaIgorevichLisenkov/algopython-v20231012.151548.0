867666630549c3d181f916a3e38d8cbbb877a55c
Serёga Lisenkov <99216176+SeregaIgorevichLisenkov@users.noreply.github.com>
2023-12-17 18:28:19 +0300

    Update pip_install.py

--- pip_install.py
+++ pip_install.py
@@ -1,14 +1,15 @@
 import subprocess
 import sys
 
-# package = 'Pygame'
-# subprocess.check_call([sys.executable,'-m','pip','install',package])
-
-packages_to_install = ['Numpy','Pandas','Matplotlib','Flask','Kivy','Panda3D','Pillow','PyQt5','Pygame','','','','','']
+packages_to_install = ['Numpy','Pandas','Matplotlib','Flask','Kivy','Panda3D','Pillow','PyQt5','Pygame','pytesseract','pyttsx3','playsound','SciPy','Seaborn','statsmodels','plotly','bokeh','scikit-learn','Requests','HTTPX','Retrying','Celery','Dramatiq','TensorFlow','Keras','PyTorch','Pymorphy2','Pymystem3','OpenCV','rich','loguru','pydantic','dateparser','Py-spy','Pympler','Responses','Freezegun','Faker','Factory_boy','Funcy','tqdm','black',]
 
 for package in packages_to_install:
     subprocess.check_call([sys.executable,'-m','pip','install',package])
 
+# msiexec /a python-3.6.msi TARGETDIR=r: \python36 ALLUSERS=1
+# subprocess.check_call([sys.executable,'-m','pip','install',package])
+
+#########################################
 # import pip
 # def install(package):
 #     pip.main(['install',package])
