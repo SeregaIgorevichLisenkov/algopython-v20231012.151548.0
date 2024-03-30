c19e62c7bb3e296a58a312d98ee07c178f503989
Serёga Lisenkov <99216176+SeregaIgorevichLisenkov@users.noreply.github.com>
2024-03-30 17:09:42 +0300

    Update pip_install.py

--- pip_install.py
+++ pip_install.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-packages_to_install = ['Numpy','Pandas','Matplotlib','Flask','Kivy','Panda3D','Pillow','PyQt5','Pygame','pytesseract','pyttsx3','playsound','SciPy','Seaborn','statsmodels','plotly','bokeh','scikit-learn','Requests','HTTPX','Retrying','Celery','Dramatiq','TensorFlow','Keras','PyTorch','Pymorphy2','Pymystem3','OpenCV','rich','loguru','pydantic','dateparser','Py-spy','Pympler','Responses','Freezegun','Faker','Factory_boy','Funcy','tqdm','black',]
+packages_to_install = ['pyinstaller','Numpy','Pandas','Matplotlib','Flask','Kivy','Panda3D','Pillow','PyQt5','Pygame','pytesseract','pyttsx3','playsound','SciPy','Seaborn','statsmodels','plotly','bokeh','scikit-learn','Requests','HTTPX','Retrying','Celery','Dramatiq','TensorFlow','Keras','PyTorch','Pymorphy2','Pymystem3','OpenCV','rich','loguru','pydantic','dateparser','Py-spy','Pympler','Responses','Freezegun','Faker','Factory_boy','Funcy','tqdm','black',]
 
 for package in packages_to_install:
     subprocess.check_call([sys.executable,'-m','pip','install',package])
